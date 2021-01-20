5bc63b548d91ab32ef9e2dc1eff956a460700bf4
41275 <41275@heb.be>
2021-01-20 02:37:43 +0100

    create main method

---
+++ src/main/java/tennis/kata/TennisMain.java
@@ -0,0 +1,11 @@
+package tennis.kata;
+
+import tennis.kata.model.TennisGame;
+import tennis.kata.view.TennisGameView;
+
+public class TennisMain {
+    public static void main(String[] args) {
+        TennisGameView tennisGameView=new TennisGameView();
+        tennisGameView.launch();
+    }
+}
