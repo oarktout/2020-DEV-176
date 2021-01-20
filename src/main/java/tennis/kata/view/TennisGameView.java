87e766a126fe1d407f9edd5a719ad01cf229682e
41275 <41275@heb.be>
2021-01-20 02:22:21 +0100

    Crea

---
+++ src/main/java/tennis/kata/view/TennisGameView.java
@@ -0,0 +1,10 @@
+package tennis.kata.view;
+
+import tennis.kata.controller.TennisGameController;
+
+public class TennisGameView {
+    private TennisGameController gameController;
+
+    public TennisGameView(){
+    }
+}
