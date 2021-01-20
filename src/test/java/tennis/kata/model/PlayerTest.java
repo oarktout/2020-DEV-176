445cf2b274d1f461d38c8853ae44bd8a28b227f1
41275 <41275@heb.be>
2021-01-20 01:28:03 +0100

    refactoring test to add method to initialize player before the test and beginning with correct value for each test

--- src/test/java/tennis/kata/model/PlayerTest.java
+++ src/test/java/tennis/kata/model/PlayerTest.java
@@ -1,11 +1,22 @@
 package tennis.kata.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class PlayerTest {
     private Player player;
+
+    @BeforeEach
+    void initializePlayer(){
+        player=new Player("Test");
+        assertEquals("Test",player.getName());
+        assertEquals(0,player.getScore());
+        assertEquals(0,player.getScoreGame());
+        assertFalse(player.isAdvantage());
+    }
 
     @Test
     void wonAGame() {
